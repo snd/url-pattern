d409ab218b0a4df862b2730813a16eea4c705c6e
Maximilian Krüger <kruemaxi@googlemail.com>
2015-09-01 18:34:40 +0200

    fix bash script run-travis-tests.sh

--- run-travis-tests.sh
+++ run-travis-tests.sh
@@ -4,9 +4,9 @@
 # v = verbose
 set -ev
 
-if [ $ZUUL = 'true' ]; then
+if [[ $ZUUL == 'true' ]] then
   npm run test-zuul;
-elif [ $COVERAGE = 'true' ]; then
+elif [[ $COVERAGE == 'true' ]] then
   npm run test-with-codecov;
 else
   npm test;

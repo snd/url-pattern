72b354725312e367575fc86e127476b876c21c30
Mikael Waltersson <mikael.waltersson@gmail.com>
2016-11-08 15:23:32 +1100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ interface UrlPatternOptions {
     segmentNameCharset?: string;
     optionalSegmentStartChar?: string;
     optionalSegmentEndChar?: string;
-    wildcardChar: string;
+    wildcardChar?: string;
 }
 
 declare class UrlPattern {

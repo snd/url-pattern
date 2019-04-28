d70210b2806dae5d6f146b3900f4fd95c9273074
Maximilian Krüger <kruemaxi@gmail.com>
2019-04-27 23:38:52 -0500

    make test/misc.js pass

--- test/misc.js
+++ test/misc.js
@@ -4,17 +4,19 @@
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 const test = require('tape');
-const UrlPattern = require('../lib/url-pattern');
+const {
+  UrlPattern
+} = require('../index.js');
 
 test('instance of UrlPattern is handled correctly as constructor argument', function(t) {
-    const pattern = new UrlPattern('/user/:userId/task/:taskId');
-    const copy = new UrlPattern(pattern);
-    t.deepEqual(copy.match('/user/10/task/52'), {
-      userId: '10',
-      taskId: '52'
-    }
-    );
-    return t.end();
+  const pattern = new UrlPattern('/user/:userId/task/:taskId');
+  const copy = new UrlPattern(pattern);
+  t.deepEqual(copy.match('/user/10/task/52'), {
+    userId: '10',
+    taskId: '52'
+  }
+  );
+  t.end();
 });
 
 test('match full stops in segment values', function(t) {
@@ -23,20 +25,20 @@ test('match full stops in segment values', function(t) {
   const pattern = new UrlPattern('/api/v1/user/:id/', options);
   t.deepEqual(pattern.match('/api/v1/user/test.name/'),
     {id: 'test.name'});
-  return t.end();
+  t.end();
 });
 
 test('regex group names', function(t) {
   const pattern = new UrlPattern(/^\/api\/([a-zA-Z0-9-_~ %]+)(?:\/(\d+))?$/, ['resource', 'id']);
   t.deepEqual(pattern.match('/api/users'),
     {resource: 'users'});
-  t.equal(pattern.match('/apiii/users'), null);
-  t.deepEqual(pattern.match('/api/users/foo'), null);
+  t.equal(pattern.match('/apiii/users'), undefined);
+  t.deepEqual(pattern.match('/api/users/foo'), undefined);
   t.deepEqual(pattern.match('/api/users/10'), {
     resource: 'users',
     id: '10'
   }
   );
-  t.deepEqual(pattern.match('/api/projects/10/'), null);
-  return t.end();
+  t.deepEqual(pattern.match('/api/projects/10/'), undefined);
+  t.end();
 });

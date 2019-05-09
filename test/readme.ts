4d7ffa729bb61fcc54b7567b82cd7a02dce0dd6b
Maximilian Krüger <kruemaxi@gmail.com>
2019-05-09 15:51:34 -0500

    make readme tests pass

--- test/readme.ts
+++ test/readme.ts
@@ -5,14 +5,15 @@ import * as tape from "tape";
 import UrlPattern from "../src/url-pattern";
 
 tape("match a pattern against a string and extract values", (t: tape.Test) => {
-  const pattern = new UrlPattern("/api/users/:id");
+  const pattern = new UrlPattern("/api/users(/:id)");
   t.deepEqual(pattern.match("/api/users/10"), {id: "10"});
+  t.deepEqual(pattern.match("/api/users"), {});
   t.equal(pattern.match("/api/products/5"), undefined);
   t.end();
 });
 
 tape("generate a string from a pattern and values", (t: tape.Test) => {
-  const pattern = new UrlPattern("/api/users/:id");
+  const pattern = new UrlPattern("/api/users(/:id)");
   t.equal(pattern.stringify(), "/api/users");
   t.equal(pattern.stringify({id: 20}), "/api/users/20");
   t.end();
@@ -29,7 +30,7 @@ tape("prefer a different syntax. customize it", (t: tape.Test) => {
     options,
   );
 
-  t.deepEqual(pattern.match("/users/5"), {
+  t.deepEqual(pattern.match("/api/users/5"), {
     id: "5",
   });
   t.end();
